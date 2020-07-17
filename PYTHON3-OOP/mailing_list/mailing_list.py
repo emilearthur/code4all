bbf58cce9f65e6da5a4dc880e8886db611b868be
fredrickemile <frederickauthur@hotmail.com>
2020-07-17 15:52:22 +0000

    update

--- PYTHON3-OOP/mailing_list/mailing_list.py
+++ PYTHON3-OOP/mailing_list/mailing_list.py
@@ -8,6 +8,7 @@ address assigned to that group.
 import smtplib
 from email.mime.text import MIMEText
 from collections import defaultdict
+from contextlib import suppress
 
 def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=1025, headers=None):
     headers = headers if headers else {}
@@ -26,11 +27,14 @@ def send_email(subject, message, from_addr, *to_addrs, host="localhost", port=10
 
 class MailingList:
     """Manage groups of e-mail addresses for sending e-mails."""
-    def __init__(self):
+    def __init__(self, data_file):
+        self.data_file = data_file
         self.email_map = defaultdict(set)
+
 
     def add_to_group(self, email, group):
         self.email_map[email].add(group)
+
 
     def emails_in_groups(self, *groups):
         groups = set(groups) 
@@ -40,6 +44,32 @@ class MailingList:
                 emails.add(email) 
         return emails 
 
+
     def send_mailing(self, subject, message, from_addr, *groups, headers=None):
         emails = self.emails_in_groups(*groups) 
         send_email(subject, message, from_addr, *emails, headers=headers)
+
+
+    def save(self):
+        with open(self.data_file, "w") as file:
+            for email, groups in self.email_map.items():
+                file.write("{} {}\n".format(email, ",".join(groups)))
+
+
+    def load(self):
+        self.email_map = defaultdict(set) 
+        with suppress(IOError):
+            with open(self.data_file) as file:
+                for line in file:
+                    email, groups = line.strip().split(" ")
+                    groups = set(groups.split(","))
+                    self.email_map[email] = groups
+    
+
+    def __enter__(self):
+        self.load()
+        return self 
+
+    
+    def __exit__(self, type, value, tb):
+        self.save()
