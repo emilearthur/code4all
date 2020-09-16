901f84503b4820b1f9af550ba3e5bb3443607909
fredrickemile <frederickauthur@hotmail.com>
2020-09-16 21:19:11 +0000

    update

---
+++ PYTHON3-OOP/test_12_1.py
@@ -0,0 +1,9 @@
+"""
+Testing OO Programs: 
+Testing is one of the most important aspect of software dev. 
+Python test tests rarely check types as compared to C++ and Java. Python's interactive interpreter and near-zero compile 
+times makes it easy to write a few lines of code and run the program to make sure those lines are doing what is expected. 
+But changing a few lines of code affect parts we have not realized yet. Also, as the program grows it is difficult to 
+manually test all of them. To handle such issue we write automated tests. These programs that automatically run certain 
+inputs 
+"""
