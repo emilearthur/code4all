a8ed6fb039203594cfdde9af0ca5109280a6f8b6
emilearthur <frederickauthur@hotmail.com>
2021-02-23 22:53:52 +0000

    done with CRUD

--- fastapi/blog_/blog/main.py
+++ fastapi/blog_/blog/main.py
@@ -1,6 +1,10 @@
 # type: ignore
 import fastapi
 from typing import List, Dict
+from pydantic.main import create_model
+import datetime
+
+from sqlalchemy.engine import create_engine
 import blog
 from .models import schemas
 from .models import models
@@ -23,32 +27,63 @@ def get_db():
         db.close()
 
 
+@app.get('/')
+def index():
+    return "Welcome to the blog"
+
+
+# create a blog
 @app.post('/blog', name='add_blog', status_code=status.HTTP_201_CREATED)
 async def create(blog: schemas.Blog, db: Session = Depends(get_db)) -> models.Blog:
+    now = datetime.datetime.now()
     new_blog = models.Blog(title=blog.title, 
                             body=blog.body, 
                             published=blog.published, 
-                            created_at=blog.created_at,)
+                            created_at=now,)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
 
+# delete a blog
 @app.delete('/blog/{id}', name="delete_blog", status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+async def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {id} not found in db, thus cannot deleted")
+    
+    blog.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+
+@app.put('/blog/{id}',  name="update_blog", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {id} not found in db, thus cannot updated")
+
+    now = datetime.datetime.now()
+    blog.update({'title': request.title,
+                 'body': request.body,
+                 'published': request.published,
+                 'created_at': now},
+                 synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_202_ACCEPTED)
+
+
+# get all blogs in db
 @app.get('/blog', name='all_blogs')
 async def blogs_all(db: Session = Depends(get_db)) -> List:
     blogs = db.query(models.Blog).all()
     return blogs
 
 
+# get a blog with blog id
 @app.get('/blog/{id}', name='show_blog', status_code=200)
-async def show_blog(id, response: Response, db: Session = Depends(get_db)) -> Dict:
+async def show_blog(id: int, response: Response, db: Session = Depends(get_db)) -> Dict:
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         # response.status_code = status.HTTP_404_NOT_FOUND
